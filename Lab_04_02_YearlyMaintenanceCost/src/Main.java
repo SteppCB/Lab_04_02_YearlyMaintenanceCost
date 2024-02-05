2805fd4b7898c020bd18cc8260b7e6414e916275
caleb <calebstepp25@gmail.com>
2024-02-04 22:51:07 -0500

    Initial commit

---
+++ Lab_04_02_YearlyMaintenanceCost/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args)
+    {
+	double summerCost = 245.76;
+    double fallCost = 643.29;
+    double winterCost = 694.32;
+    double springCost = 854.43;
+
+    double yearlyCost = summerCost + fallCost + winterCost + springCost;
+        System.out.println("The maintenance cost for the summer is $" + summerCost + " and the maintenance cost for the fall is $" + fallCost + " and the maintenance cost for the winter is $" + winterCost + " and the maintenance cost for spring is $" + springCost + ". The total maintenance cost for the whole year is $" + yearlyCost);
+
+    }
+}
